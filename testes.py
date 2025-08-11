9151bd3c15f307a7666af553b05b7352a151d03f
thais-collab <thais.adryene2056@gmail.com>
2025-08-11 16:36:34 -0300

    Add files via upload

---
+++ testes.py
@@ -0,0 +1,44 @@
+print("Ola Mundo!")
+# Saida esperada: Ola Mundo!
+
+Thais = input ("Qual o seu nome?")
+print("Ola Thais")
+Sim = input ("tudo bem com voce?")
+print("Que bom!")
+# Tudo ok!
+
+print ("Eu sei fazer calculos matematicos, ex. 80+50...")
+Sim = input ("voce quer ver a soma desses valores?")
+Num1 = 80
+Num2 = 50
+Soma = Num1 + Num2
+print("O resultado e:", Soma)
+# Tudo ok!
+
+Variavel = 100
+print("O numero informado foi:", Variavel)
+# tudo ok
+
+nome = Thais
+Sobrenome = ("Adryene")
+Variavel3 = 18
+print ("nome:", nome)
+print("sobrenome:", Sobrenome)
+print(" idade:", Variavel3)
+# tudo ok
+
+filme = ("Jonh_Wick")
+genero = ("acao")  
+print ("fime:", filme)
+print("genero:", genero)
+# tudo ok
+
+palavra1 = ("chuva,")
+palavra2 = ("chinelo,")
+palavra3 = ("casa")
+soma = palavra1 + palavra2 + palavra3
+print("as palavras sao:", soma)
+#tudo ok
+
+print("Camaragibe.")
+print("Pernambuco")
